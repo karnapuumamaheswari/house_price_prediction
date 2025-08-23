a68d043a348cdb57b38a906a869783b202637c1d
karnapuumamaheswari <karnapuumamaheswari022005@gmail.com>
2025-08-23 10:34:06 +0530

    modified server.py for render deployment

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, jsonify
+import util
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "House Price Prediction App is Running!"
+    return render_template('index.html')
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    if request.method == 'POST':
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
+    else:  # GET request (browser query params)
+        total_sqft = float(request.args.get('total_sqft'))
+        location = request.args.get('location')
+        bhk = int(request.args.get('bhk'))
+        bath = int(request.args.get('bath'))
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ static/app.js
@@ -0,0 +1,68 @@
+
+
+function getBathValue() {
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for (var i = 0; i < uiBathrooms.length; i++) {
+    if (uiBathrooms[i].checked) {
+      return parseInt(uiBathrooms[i].value);
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getBHKValue() {
+  var uiBHK = document.getElementsByName("uiBHK");
+  for (var i = 0; i < uiBHK.length; i++) {
+    if (uiBHK[i].checked) {
+      return parseInt(uiBHK[i].value);
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
+  console.log("Estimate price button clicked");
+
+  var sqft = document.getElementById("uiSqft");
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations");
+  var estPrice = document.getElementById("uiEstimatedPrice");
+
+  var url = "http://127.0.0.1:5000/predict_home_price"; // Flask backend
+
+  $.post(url, {
+    total_sqft: parseFloat(sqft.value),
+    bhk: bhk,
+    bath: bathrooms,
+    location: location.value
+  }, function (data, status) {
+    console.log("API Response:", data);
+    if ("estimated_price" in data) {
+      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    } else {
+      estPrice.innerHTML = "<h2>Error: No price returned</h2>";
+    }
+    console.log("Status:", status);
+  });
+}
+
+function onPageLoad() {
+  console.log("document loaded");
+  var url = "http://127.0.0.1:5000/get_location_names";
+
+  $.get(url, function (data, status) {
+    console.log("Got response for get_location_names request");
+    if (data && data.locations) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      for (var i = 0; i < locations.length; i++) {
+        var opt = new Option(locations[i]);
+        $('#uiLocations').append(opt);
+      }
+    }
+  });
+}
+
+window.onload = onPageLoad;
